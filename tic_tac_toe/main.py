427b05566215be661e1f733bb63119f770d93af5
Sergey Ilin <ilin_sergey90@mail.ru>
2023-11-22 07:30:53 +0300

    field

--- tic_tac_toe/main.py
+++ tic_tac_toe/main.py
@@ -8,9 +8,21 @@ pygame.display.set_caption('TIC TAC TO')
 icon = pygame.image.load('images/icon.png')
 pygame.display.set_icon(icon)
 
-is_game_active = True
 
+is_game_active = True
 while is_game_active:
+    pygame.draw.line(surface=screen, color='White',
+                     start_pos=(0, 198), end_pos=(600, 198),
+                     width=5)
+    pygame.draw.line(surface=screen, color='White',
+                     start_pos=(0, 396), end_pos=(600, 396),
+                     width=5)
+    pygame.draw.line(surface=screen, color='White',
+                     start_pos=(198, 0), end_pos=(198, 600),
+                     width=5)
+    pygame.draw.line(surface=screen, color='White',
+                     start_pos=(396, 0), end_pos=(396, 600),
+                     width=5)
 
     pygame.display.update()
 
