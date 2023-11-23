1a2924cdeec2fc6d21bcd55224e7525f6b0b8fbb
Sergey Ilin <ilin_sergey90@mail.ru>
2023-11-23 22:07:49 +0300

    шрифт и вывод сообщения о закончившихся ходах

--- tic_tac_toe/main.py
+++ tic_tac_toe/main.py
@@ -11,23 +11,17 @@ pygame.display.set_icon(icon)
 background = pygame.image.load('images/background.png')
 screen.blit(source=background, dest=(0, 0))
 
+font = pygame.font.Font('fonts/cuyabra-Regular.ttf', 60)
+text = font.render('Ходы закончилиcь!', True, pygame.Color('red'))
+
 circle_image = pygame.image.load('images/circle.png')
 cross_image = pygame.image.load('images/cross.png')
 
 is_game_active = True
+
+fields = dict()
+
 while is_game_active:
-    # pygame.draw.line(surface=screen, color='Black',
-    #                  start_pos=(0, 200), end_pos=(600, 200),
-    #                  width=5)
-    # pygame.draw.line(surface=screen, color='Black',
-    #                  start_pos=(0, 400), end_pos=(600, 400),
-    #                  width=5)
-    # pygame.draw.line(surface=screen, color='Black',
-    #                  start_pos=(200, 0), end_pos=(200, 600),
-    #                  width=5)
-    # pygame.draw.line(surface=screen, color='Black',
-    #                  start_pos=(400, 0), end_pos=(400, 600),
-    #                  width=5)
 
     pygame.display.update()
 
@@ -35,24 +29,36 @@ while is_game_active:
         if event.type == pygame.QUIT:
             is_game_active = False
             pygame.quit()
+
         if event.type == pygame.MOUSEBUTTONDOWN:
             position = pygame.mouse.get_pos()
+            if len(fields) == 9:
+                screen.blit(source=text, dest=(20, 270))
             figure = cross_image
-            if 0 < position[0] < 200 and 0 < position[1] < 200:  # Поле 1
+            if 0 < position[0] < 200 and 0 < position[1] < 200 and '1' not in fields:  # Поле 1
                 screen.blit(source=figure, dest=(3, 3))
-            elif 200 < position[0] < 400 and 0 < position[1] < 200:  # Поле 2
+                fields['1'] = True
+            elif 200 < position[0] < 400 and 0 < position[1] < 200 and '2' not in fields:  # Поле 2
                 screen.blit(source=figure, dest=(203, 3))
-            elif 400 < position[0] < 600 and 0 < position[1] < 200:  # Поле 3
+                fields['2'] = True
+            elif 400 < position[0] < 600 and 0 < position[1] < 200 and '3' not in fields:  # Поле 3
                 screen.blit(source=figure, dest=(403, 3))
-            elif 0 < position[0] < 200 and 200 < position[1] < 400:  # Поле 4
+                fields['3'] = True
+            elif 0 < position[0] < 200 and 200 < position[1] < 400 and '4' not in fields:  # Поле 4
                 screen.blit(source=figure, dest=(3, 203))
-            elif 200 < position[0] < 400 and 200 < position[1] < 400:  # Поле 5
+                fields['4'] = True
+            elif 200 < position[0] < 400 and 200 < position[1] < 400 and '5' not in fields:  # Поле 5
                 screen.blit(source=figure, dest=(203, 203))
-            elif 400 < position[0] < 600 and 200 < position[1] < 400:  # Поле 6
+                fields['5'] = True
+            elif 400 < position[0] < 600 and 200 < position[1] < 400 and '6' not in fields:  # Поле 6
                 screen.blit(source=figure, dest=(403, 203))
-            elif 0 < position[0] < 200 and 400 < position[1] < 600:  # Поле 7
+                fields['6'] = True
+            elif 0 < position[0] < 200 and 400 < position[1] < 600 and '7' not in fields:  # Поле 7
                 screen.blit(source=figure, dest=(3, 403))
-            elif 200 < position[0] < 400 and 400 < position[1] < 600:  # Поле 8
+                fields['7'] = True
+            elif 200 < position[0] < 400 and 400 < position[1] < 600 and '8' not in fields:  # Поле 8
                 screen.blit(source=figure, dest=(203, 403))
-            elif 400 < position[0] < 600 and 400 < position[1] < 600:  # Поле 9
+                fields['8'] = True
+            elif 400 < position[0] < 600 and 400 < position[1] < 600 and '9' not in fields:  # Поле 9
                 screen.blit(source=figure, dest=(403, 403))
+                fields['9'] = True
