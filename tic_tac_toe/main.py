9a27b48539492dd52b9c38fe0b2957918eb1455c
Sergey Ilin <ilin_sergey90@mail.ru>
2023-11-25 13:14:12 +0300

    изменил логику смены игрока

--- tic_tac_toe/main.py
+++ tic_tac_toe/main.py
@@ -1,3 +1,5 @@
+from random import choice
+
 import pygame
 
 
@@ -69,6 +71,24 @@ def current_player(figure) -> str:
         return 'X'
 
 
+def computer_step() -> str:
+    free_fields = [field for field in fields.keys() if fields[field]]
+    step = choice(free_fields)
+    return step
+
+
+def drow_figure():
+    pass
+
+
+def change_figure():
+    global figure
+    if figure == figures['O']:
+        figure = figures['X']
+    elif figure == figures['X']:
+        figure = figures['O']
+
+
 is_game_active = True
 
 while is_game_active:
@@ -84,33 +104,42 @@ while is_game_active:
         if event.type == pygame.MOUSEBUTTONDOWN:
             position = pygame.mouse.get_pos()
 
-            if 0 < position[0] < 200 and 0 < position[1] < 200:  # Поле 1
+            if 0 < position[0] < 200 and 0 < position[1] < 200 and not fields['1']:  # Поле 1
                 screen.blit(source=figure, dest=(3, 3))
                 fields['1'] = current_player(figure)
-            elif 200 < position[0] < 400 and 0 < position[1] < 200:  # Поле 2
+                change_figure()
+            elif 200 < position[0] < 400 and 0 < position[1] < 200 and not fields['2']:  # Поле 2
                 screen.blit(source=figure, dest=(203, 3))
                 fields['2'] = current_player(figure)
-            elif 400 < position[0] < 600 and 0 < position[1] < 200:  # Поле 3
+                change_figure()
+            elif 400 < position[0] < 600 and 0 < position[1] < 200 and not fields['3']:  # Поле 3
                 screen.blit(source=figure, dest=(403, 3))
                 fields['3'] = current_player(figure)
-            elif 0 < position[0] < 200 and 200 < position[1] < 400:  # Поле 4
+                change_figure()
+            elif 0 < position[0] < 200 and 200 < position[1] < 400 and not fields['4']:  # Поле 4
                 screen.blit(source=figure, dest=(3, 203))
                 fields['4'] = current_player(figure)
-            elif 200 < position[0] < 400 and 200 < position[1] < 400:  # Поле 5
+                change_figure()
+            elif 200 < position[0] < 400 and 200 < position[1] < 400 and not fields['5']:  # Поле 5
                 screen.blit(source=figure, dest=(203, 203))
                 fields['5'] = current_player(figure)
-            elif 400 < position[0] < 600 and 200 < position[1] < 400:  # Поле 6
+                change_figure()
+            elif 400 < position[0] < 600 and 200 < position[1] < 400 and not fields['6']:  # Поле 6
                 screen.blit(source=figure, dest=(403, 203))
                 fields['6'] = current_player(figure)
-            elif 0 < position[0] < 200 and 400 < position[1] < 600:  # Поле 7
+                change_figure()
+            elif 0 < position[0] < 200 and 400 < position[1] < 600 and not fields['7']:  # Поле 7
                 screen.blit(source=figure, dest=(3, 403))
                 fields['7'] = current_player(figure)
-            elif 200 < position[0] < 400 and 400 < position[1] < 600:  # Поле 8
+                change_figure()
+            elif 200 < position[0] < 400 and 400 < position[1] < 600 and not fields['8']:  # Поле 8
                 screen.blit(source=figure, dest=(203, 403))
                 fields['8'] = current_player(figure)
-            elif 400 < position[0] < 600 and 400 < position[1] < 600:  # Поле 9
+                change_figure()
+            elif 400 < position[0] < 600 and 400 < position[1] < 600 and not fields['9']:  # Поле 9
                 screen.blit(source=figure, dest=(403, 403))
                 fields['9'] = current_player(figure)
+                change_figure()
 
             if len(fields) > 5:  # Проверка выигрыша
                 result = check_win()
@@ -119,8 +148,3 @@ while is_game_active:
 
             if all(fields.values()):  # Проверка на кроичество свободных полей
                 screen.blit(source=text, dest=(20, 270))
-
-            if figure == figures['O']:
-                figure = figures['X']
-            elif figure == figures['X']:
-                figure = figures['O']
