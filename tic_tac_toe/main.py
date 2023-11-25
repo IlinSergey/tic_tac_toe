536d0e2a844f8cd26346924900b1c89ef1e99b54
Sergey Ilin <ilin_sergey90@mail.ru>
2023-11-25 13:54:03 +0300

    подключил ход компьютера с рандомным выбором

--- tic_tac_toe/main.py
+++ tic_tac_toe/main.py
@@ -1,4 +1,5 @@
 from random import choice
+from time import sleep
 
 import pygame
 
@@ -72,13 +73,25 @@ def current_player(figure) -> str:
 
 
 def computer_step() -> str:
-    free_fields = [field for field in fields.keys() if fields[field]]
+    sleep(0.5)
+    free_fields = [field for field in fields.keys() if fields[field] is False]
     step = choice(free_fields)
     return step
 
 
-def drow_figure():
-    pass
+def drow_figure(figure, field):
+    coordinates = {
+        '1': (3, 3),
+        '2': (203, 3),
+        '3': (403, 3),
+        '4': (3, 203),
+        '5': (203, 203),
+        '6': (403, 203),
+        '7': (3, 403),
+        '8': (203, 403),
+        '9': (403, 403),
+    }
+    screen.blit(source=figure, dest=coordinates[field])
 
 
 def change_figure():
@@ -97,6 +110,12 @@ while is_game_active:
 
     for event in pygame.event.get():
 
+        if figure == figures['X']:
+            step = computer_step()
+            drow_figure(figure=figure, field=step)
+            fields[step] = current_player(figure)
+            change_figure()
+
         if event.type == pygame.QUIT:
             is_game_active = False
             pygame.quit()
@@ -105,39 +124,39 @@ while is_game_active:
             position = pygame.mouse.get_pos()
 
             if 0 < position[0] < 200 and 0 < position[1] < 200 and not fields['1']:  # Поле 1
-                screen.blit(source=figure, dest=(3, 3))
+                drow_figure(figure, field='1')
                 fields['1'] = current_player(figure)
                 change_figure()
             elif 200 < position[0] < 400 and 0 < position[1] < 200 and not fields['2']:  # Поле 2
-                screen.blit(source=figure, dest=(203, 3))
+                drow_figure(figure, field='2')
                 fields['2'] = current_player(figure)
                 change_figure()
             elif 400 < position[0] < 600 and 0 < position[1] < 200 and not fields['3']:  # Поле 3
-                screen.blit(source=figure, dest=(403, 3))
+                drow_figure(figure, field='3')
                 fields['3'] = current_player(figure)
                 change_figure()
             elif 0 < position[0] < 200 and 200 < position[1] < 400 and not fields['4']:  # Поле 4
-                screen.blit(source=figure, dest=(3, 203))
+                drow_figure(figure, field='4')
                 fields['4'] = current_player(figure)
                 change_figure()
             elif 200 < position[0] < 400 and 200 < position[1] < 400 and not fields['5']:  # Поле 5
-                screen.blit(source=figure, dest=(203, 203))
+                drow_figure(figure, field='5')
                 fields['5'] = current_player(figure)
                 change_figure()
             elif 400 < position[0] < 600 and 200 < position[1] < 400 and not fields['6']:  # Поле 6
-                screen.blit(source=figure, dest=(403, 203))
+                drow_figure(figure, field='6')
                 fields['6'] = current_player(figure)
                 change_figure()
             elif 0 < position[0] < 200 and 400 < position[1] < 600 and not fields['7']:  # Поле 7
-                screen.blit(source=figure, dest=(3, 403))
+                drow_figure(figure, field='7')
                 fields['7'] = current_player(figure)
                 change_figure()
             elif 200 < position[0] < 400 and 400 < position[1] < 600 and not fields['8']:  # Поле 8
-                screen.blit(source=figure, dest=(203, 403))
+                drow_figure(figure, field='8')
                 fields['8'] = current_player(figure)
                 change_figure()
             elif 400 < position[0] < 600 and 400 < position[1] < 600 and not fields['9']:  # Поле 9
-                screen.blit(source=figure, dest=(403, 403))
+                drow_figure(figure, field='9')
                 fields['9'] = current_player(figure)
                 change_figure()
 
@@ -146,5 +165,5 @@ while is_game_active:
                 if result:
                     screen.blit(source=win_text[result], dest=(20, 270))
 
-            if all(fields.values()):  # Проверка на кроичество свободных полей
+            if all(fields.values()):  # Проверка на количество свободных полей
                 screen.blit(source=text, dest=(20, 270))
